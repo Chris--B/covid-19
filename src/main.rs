2c063d8cae1858a4658566680a46fc97f3d91508
Chris Butler <chrisbutler296@gmail.com>
2020-03-15 18:44:06 -0400

    Empty ImGui rs project!

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,103 @@
-use covid_sim;
+use gl;
+use nalgebra;
+
+use std::time::Instant;
+
+// We need our dependencies to match exactly,
+// so turn this off and use the private export directly.
+#[allow(pub_use_of_private_extern_crate)]
+use imgui_sdl2::imgui;
+
+fn build_ui(ui: &imgui::Ui) {
+    ui.show_demo_window(&mut true);
+}
 
 fn main() {
-    let me = covid_sim::Person::default();
-    println!("Hello world, {:#?}", me);
+    use imgui_opengl_renderer::Renderer;
+    use imgui_sdl2::ImguiSdl2;
 
-    Window::new(im_str!("Hello world"))
-        .size([300.0, 100.0], Condition::FirstUseEver)
-        .build(&ui, || {
-            ui.text(im_str!("Hello world!"));
-            ui.text(im_str!("こんにちは世界！"));
-            ui.text(im_str!("This...is...imgui-rs!"));
-            ui.separator();
-            let mouse_pos = ui.io().mouse_pos;
-            ui.text(format!(
-                "Mouse Position: ({:.1},{:.1})",
-                mouse_pos[0], mouse_pos[1]
-            ));
-        });
+    let sdl_context = sdl2::init().unwrap();
+    let video = sdl_context.video().unwrap();
+
+    {
+        let gl_attr = video.gl_attr();
+        gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+        gl_attr.set_context_version(3, 0);
+    }
+
+    let window = video
+        .window("rust-imgui-sdl2 demo", 1000, 1000)
+        .position_centered()
+        .resizable()
+        .opengl()
+        .allow_highdpi()
+        .build()
+        .unwrap();
+
+    let _gl_context = window
+        .gl_create_context()
+        .expect("Couldn't create GL context");
+    gl::load_with(|s| video.gl_get_proc_address(s) as _);
+
+    let mut imgui = imgui::Context::create();
+    let mut imgui_sdl2 = ImguiSdl2::new(&mut imgui, &window);
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let renderer = Renderer::new(&mut imgui, |s| video.gl_get_proc_address(s) as _);
+    let mut last_frame = Instant::now();
+
+    'running: loop {
+        use sdl2::event::Event;
+        use sdl2::keyboard::Keycode;
+
+        for event in event_pump.poll_iter() {
+            imgui_sdl2.handle_event(&mut imgui, &event);
+            if imgui_sdl2.ignore_event(&event) {
+                continue;
+            }
+
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Q),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        imgui_sdl2.prepare_frame(imgui.io_mut(), &window, &event_pump.mouse_state());
+
+        let now = Instant::now();
+        let delta = now - last_frame;
+        let delta_s = delta.as_secs() as f32 + delta.subsec_nanos() as f32 / 1_000_000_000.0;
+        last_frame = now;
+        imgui.io_mut().delta_time = delta_s;
+
+        // Update state
+
+        // Build UI
+        let ui = imgui.frame();
+        build_ui(&ui);
+
+        unsafe {
+            gl::ClearColor(0.2, 0.2, 0.2, 1.0);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
+        // Render non-UI things
+
+        // Render UI
+        imgui_sdl2.prepare_render(&ui, &window);
+        renderer.render(ui);
+
+        // Present
+        window.gl_swap_window();
+
+        // Sleep to hit our target framerate.
+        ::std::thread::sleep(::std::time::Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
